c71e1ca24da66e7ac52667b9898e429a2c01281a
Knyazb <Knyazb.serg@mail.ru>
2024-03-21 00:29:00 +0300

    add old code form git test

--- Lesson 3.1.py
+++ Lesson 3.1.py
@@ -3,10 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import math
-
-def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
 
 try:
     browser = webdriver.Chrome()
